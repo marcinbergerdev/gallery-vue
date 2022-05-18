15c995842bc6303ba5e6193f9bd9d43a3638aeff
Your Name~ <berdzi53@gmail.com>
2022-05-18 19:01:41 +0200

    Added home page
    added and styled responsive home page design

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-<div></div>
+<home-page></home-page>
 </template>
 
 <script>
-
+import HomePage from "./components/mobile homepage/HomePage.vue";
 
 export default {
-
+components: {
+   HomePage
+}
 }
 </script>
 
-<style>
+<style lang="scss">
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root {
+  font-size: 10px;
+  font-family: "Roboto", sans-serif;
+}
 
 </style>
---
+++ src/components/mobile homepage/HomePage.vue
@@ -0,0 +1,169 @@
+<template>
+  <section class="homepage-header">
+    <img
+      class="homepage-header__mobileBg"
+      src="../../assets/backgroundMobile.jpg"
+      alt="mobile-background"
+    />
+
+    <article class="boxDecorations">
+      <div class="boxDecorations__left"></div>
+      <div class="boxDecorations__right"></div>
+    </article>
+
+    <button class="homepage-header__button">Search Gallery</button>
+  </section>
+</template>
+
+
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.homepage-header {
+  min-height: 100vh;
+  text-align: center;
+  background-color: #1e1e1e;
+  overflow: hidden;
+
+  &__mobileBg {
+    width: 100%;
+    height: 70vh;
+    filter: brightness(50%);
+  }
+
+  &__button {
+    margin-top: 100px;
+    padding: 10px;
+    background-color: #000;
+    color: #fff;
+    border: 0;
+    box-shadow: 0px 1px 17px rgb(100, 100, 100);
+  }
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+}
+
+.boxDecorations {
+  position: relative;
+
+  &__left,
+  &__right {
+    position: absolute;
+    width: 500px;
+    height: 150px;
+    background-color: #060606;
+  }
+
+  &__left {
+    top: -30px;
+    left: -244px;
+    transform: rotate(-29deg);
+  }
+
+  &__right {
+    top: -33px;
+    right: -242px;
+    transform: rotate(29deg);
+    box-shadow: -3px 2px 2px rgba(70, 70, 70, 0.26);
+  }
+
+  @media (orientation: landscape) {
+    &__left,
+    &__right {
+      height: 115px;
+    }
+
+    &__left {
+      top: -47px;
+      left: -140px;
+      transform: rotate(351deg);
+    }
+
+    &__right {
+      top: -35px;
+      right: -145px;
+      transform: rotate(13deg);
+    }
+  }
+
+  @media (min-width: 685px) and (orientation: landscape) {
+    &__left {
+      left: -106px;
+    }
+
+    &__right {
+      right: -110px;
+    }
+  }
+
+  @media (min-width: 430px) and (orientation: portrait) {
+    &__left,
+    &__right {
+      height: 160px;
+    }
+
+    &__left {
+      top: -24px;
+      left: -224px;
+    }
+
+    &__right {
+      top: -29px;
+      right: -195px;
+    }
+  }
+
+  @media (min-width: 480px) and (orientation: portrait) {
+    &__left,
+    &__right {
+      height: 170px;
+    }
+    &__right {
+      top: -42px;
+      right: -182px;
+    }
+  }
+
+  @media (min-width: 530px) and (orientation: portrait) {
+    &__left,
+    &__right {
+      height: 180px;
+    }
+
+    &__left {
+      top: -41px;
+      left: -167px;
+    }
+
+    &__right {
+      top: -48px;
+      right: -145px;
+    }
+  }
+
+  @media (min-width: 600px) and (orientation: portrait) {
+    &__left,
+    &__right {
+      width: 500px;
+      height: 180px;
+    }
+
+    &__left {
+      top: -71px;
+      left: -117px;
+      transform: rotate(-21deg);
+    }
+
+    &__right {
+      top: -63px;
+      right: -118px;
+      transform: rotate(21deg);
+    }
+  }
+}
+</style>
