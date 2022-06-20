0164987da63236d1e47294a11150e1788f09c105
Your Name~ <berdzi53@gmail.com>
2022-06-20 09:50:00 +0200

    Deleted Unused code

--- src/main.js
+++ src/main.js
@@ -5,14 +5,8 @@ import Router from './router.js';
 import App from "./App.vue";
 import i18n from './i18n'
 
-// import RegistrationCard from './components/card/RegistrationCard.vue';
-// import PhotosItemCard from './components/card/PhotosItemCard.vue';
-
-
-
-const app = createApp(App).use(i18n);
-// app.component('registration-card', RegistrationCard);
-// app.component('photos-item-card', PhotosItemCard);
-app.component('v-lazy-image',VLazyImage);
+const app = createApp(App);
 app.use(Router);
+app.use(i18n);
+app.component('v-lazy-image',VLazyImage);
 app.mount("#app");
